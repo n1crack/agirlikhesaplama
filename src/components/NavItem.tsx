50fbf3f81b51f1f53ef7176e1585dd796f2c6227
Yusuf <yusuf@ozdemir.be>
2020-04-11 14:55:38 +0300

    fix color

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -17,7 +17,7 @@ export default function NavItem({ children, onClick, selected }: Props) {
             <a
                 onClick={(e) => handleClick(e)}
                 className={
-                    "bg-white inline-block py-2 px-4 hover:text-blue-800 font-semibold " +
+                    "bg-white inline-block py-2 px-4 hover:text-red-500 font-semibold " +
                     (selected ? " text-red-500" : " text-blue-500")
                 }
                 href="#/"
